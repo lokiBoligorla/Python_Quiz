d8b1f451da8d010712df430d90ab0fb4f303a07f
Your Name <you@example.com>
2024-11-30 20:15:14 +0530

    first commit

---
+++ src/assets/data.js
@@ -0,0 +1,47 @@
+export const data=[
+    {
+        question:"Which of the following is used to define a function in Python?",
+        option1:"function",
+        option2:"fun",
+        option3:"def",
+        option4:"define",
+        ans:3
+    },
+    {
+        question:"Which data type is used to store true/false values in Python?",
+        option1:"int",
+        option2:"bool",
+        option3:"str",
+        option4:"list",
+        ans:2
+        
+
+    },
+    {
+        question:"What is the method used to add an element to the end of a list in Python?",
+        option1:"add()",
+        option2:"append()",
+        option3:"insert()",
+        option4:"extend()",
+        ans:2
+    },
+    {
+        question:"What is the output of len('Python')?",
+        option1:"5",
+        option2:"6",
+        option3:"7",
+        option4:"Error!",
+        ans:2
+    },
+    {
+        question:"What does == operator do in Python?",
+        option1:"Assignment",
+        option2:"Comparison",
+        option3:"Concatenation",
+        option4:"Logical AND",
+        ans:2
+    }
+
+
+]
+
